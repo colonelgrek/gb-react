5cdc9c3ffc4b857032029fdcb0d9817a8e47f18d
colonelgrek <colonelgrek@gmail.com>
2021-07-23 00:19:34 +0300

    add lesson3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import React from 'react';
 // import logo from './logo.svg';
+import TextField from '@material-ui/core/TextField';
 import './App.css';
 import {useEffect, useState} from "react";
 
-// const textLorem = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quis enim sint provident, odio neque repellendus? Minus saepe ipsa magni debitis delectus consequatur quam recusandae. Voluptate quia facere amet fugit?';
 const AUTHORS = {
     ME: 'Me',
     BOT: 'bot',
 }
+
+const chatList = [
+    {
+        name: 'First Chat',
+        id: 'Chat1'
+    },
+    {
+        name: 'Second Chat',
+        id: 'Chat2'
+    },
+    {
+        name: 'Third Chat',
+        id: 'Chat3'
+    },
+    {
+        name: 'Fourth Chat',
+        id: 'Chat4'
+    }
+];
 
 let id = 0;
 
@@ -16,7 +36,27 @@ function Message(props) {
     );
 }
 
-function App() {
+function ChatList(props) {
+    const { chatList } = props;
+    // const [chatList, setChatList] = useState(chatList1);
+
+    // return (
+    //     <div className="chatList">
+    //         {chatList.map( (chat) => (
+    //             <ChatName key={chat.id} text={chat.name} className={"border"}></ChatName>
+    //         ))}
+    //     </div>
+    return (
+        // <p className={chatList.id}>{chatList.name}</p>
+    <div className={props.className}>
+        {chatList.map( (chatItem) => (
+            <p key={chatItem.id}>{chatItem.name}</p>
+        ))}
+    </div>
+    );
+}
+
+function Chat() {
     const [messageList, setMessageList] = useState([]);
     const [inputValue, setInputValue] = useState('');
 
@@ -49,19 +89,39 @@ function App() {
     }
 
   return (
-      <div className="App">
+      <div>
           <div className="messages">
-              {messageList.map( (message, index) => (
-                  <Message key={message.id} text={message.text} author={message.author} className={"border"}></Message>
+              {messageList.map( (message) => (
+                  <Message key={message.id} text={message.text} author={message.author} className={"bgcYellow"}></Message>
               ))}
           </div>
 
-          <form onSubmit={handleMessageSubmit}>
-              <input value={inputValue} onChange={handleMessageChange} required/>
-              <button>Send</button>
+          <form className="app__form" onSubmit={handleMessageSubmit}>
+              <TextField
+                  fullWidth
+                  required
+                  autoFocus
+                  className="child__text-field bordered"
+                  variant="outlined"
+                  label="Сообщение"
+                  placeholder="Введите сообщение"
+                  value={inputValue}
+                  onChange={handleMessageChange}
+              />
+              <button>Отправить</button>
           </form>
       </div>
   );
+}
+
+function App() {
+
+    return (
+        <div className="App">
+            <ChatList chatList = {chatList} className={"chatList"}></ChatList>
+            <Chat/>
+        </div>
+    );
 }
 
 export default App;
